dab445347d151e5393db8b5c24e5311df37b8f84
George Watson <gigolo@hotmail.co.uk>
2024-09-11 20:07:06 +0100

    changed license to gplv3

--- src/rlut.h
+++ src/rlut.h
@@ -1,27 +1,21 @@
 /* rlut.h -- https://www.github.com/takeiteasy/rlut
  
- The MIT License (MIT)
-
- Copyright (c) 2024 George Watson
-
- Permission is hereby granted, free of charge, to any person
- obtaining a copy of this software and associated documentation
- files (the "Software"), to deal in the Software without restriction,
- including without limitation the rights to use, copy, modify, merge,
- publish, distribute, sublicense, and/or sell copies of the Software,
- and to permit persons to whom the Software is furnished to do so,
- subject to the following conditions:
-
- The above copyright notice and this permission notice shall be
- included in all copies or substantial portions of the Software.
-
- THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
- EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
- MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
- IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY
- CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
- TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
- SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE. */
+ RLUT is a utilities toolkit for making rogue-likes
+ 
+ Copyright (C) 2024  George Watson
+ 
+ This program is free software: you can redistribute it and/or modify
+ it under the terms of the GNU General Public License as published by
+ the Free Software Foundation, either version 3 of the License, or
+ (at your option) any later version.
+ 
+ This program is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
+ 
+ You should have received a copy of the GNU General Public License
+ along with this program.  If not, see <https://www.gnu.org/licenses/>. */
 
 #ifndef RLUT_HEADER
 #define RLUT_HEADER
@@ -72,7 +66,7 @@ enum {
     RLUT_HINT_DISABLE_TEXT_WRAP,
     RLUT_HINT_DISABLE_TEXT_AUTO_ADVANCE,
     RLUT_HINT_ENABLE_Y_WRAP,
-    RLUT_HINT_DISABLE_UTF8,
+    RLUT_HINT_DISABLE_UTF8, /* TODO */
     RLUT_HINT_DISABLE_RUNNING_COLOR,
     RLUT_HINT_INITIAL_SEED
 };
